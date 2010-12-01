136854fbf16127900b514525c351b70ce9e87bf9
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2010-12-01 04:29:25 +0000

    oops, missing files

---
+++ trunk/MineQuest/src/Ability.java
@@ -0,0 +1,50 @@
+
+public class Ability {
+	private String name;
+	private boolean enabled;
+	private int type;
+	
+	public Ability(String name, int type) {
+		this.name = name;
+		this.type = type;
+		enabled = false;
+	}
+	
+	public void parseAttack(Player player, Mob mob) {
+		// TODO: write Ability.parseAttack(player, mob);
+	}
+	
+	public void parseDefend(Player player, Mob mob) {
+		// TODO: write Ability.parseDefend(player, mob);
+	}
+	
+	public void enable() {
+		// TODO: write Ability.enable();
+	}
+	
+	public void disable() {
+		// TODO: write Ability.disable();
+	}
+	
+	public boolean isEnabled() {
+		// TODO: write Ability.isEnabled();
+		return false;
+	}
+	
+	public boolean equals(String name) {
+		// TODO: write Ability.equals(name);
+		return false;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	public void blockDestroy() {
+		// TODO: write Ability.blockDestroy();
+	}
+}
