a10f1f153b9208e30739fef40c4fade075e41336
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2010-12-02 06:14:17 +0000

    health won't work till hey0 update, damage working, classes working, trying to get spells going

--- MineQuest/src/SkillClass.java
+++ MineQuest/src/SkillClass.java
@@ -11,6 +11,7 @@ public class SkillClass {
 	private Ability ability_list[];
 	private String name;
 	private String type;
+	private int mana;
 	private mysql_interface sql_server;
 	
 	public SkillClass(String type, String name, mysql_interface sql) {
@@ -21,20 +22,60 @@ public class SkillClass {
 	}
 	
 	public void attack(Player player, BaseEntity defender, Quester quester) {
+		int i;
+		
 		LivingEntity defend = getLiveEnt(defender);
 		System.out.println("Call to " + name + ".attack()");
 		if (defend == null) {
 			System.out.println("oops");
 		}
-		defend.setHealth(defend.getHealth() - 10);
 		
-		exp += 10;
-		if (exp > 100 * (level + 1)) {
-			levelUp(quester);
-			player.sendMessage("Congradulations " + name + ", you are now a level " + level + " " + type);
+		for (i = 0; i < ability_list.length; i++) {
+			if (ability_list[i].isBound(player.getItemInHand())) {
+				ability_list[i].parseAttack(player, defend, quester);
+			}
 		}
+		
+		
+		defend.setHealth(defend.getHealth() - getDamage(defender.getName()));
+		
+		expAdd(getExpMob(defender.getName()), quester);
+		
 	}
-	
+
+	private int getExpMob(String name2) {
+		if (name2.equals("Creeper")) {
+			if (type.equals("Warrior")) {
+				return 10;
+			} else {
+				return 5;
+			}
+		} else if (name2.equals("Spider")) {
+			if (type.equals("WarMage")) {
+				return 10;
+			} else {
+				return 5;
+			}
+		} else if (name2.equals("Skeleton")) {
+			if (type.equals("PeaceMage")) {
+				return 10;
+			} else {
+				return 5;
+			}
+		} else if (name2.equals("Zombie")) {
+			if (type.equals("Archer")) {
+				return 10;
+			} else {
+				return 5;
+			}
+		}
+		return 3;
+	}
+
+	private int getDamage(String name2) {
+		return 2;
+	}
+
 	private LivingEntity getLiveEnt(BaseEntity defender) {
 		List<LivingEntity> entity_list = etc.getServer().getLivingEntityList();
 		int i;
@@ -51,16 +92,24 @@ public class SkillClass {
 		// TODO: write SkillClass.defend(player, mob);
 	}
 	
+	private void expAdd(int expNum, Quester quester) {
+		// TODO Auto-generated method stub
+		exp += expNum;
+		if (exp >= 400 * (level + 1)) {
+			levelUp(quester);
+		}
+	}
+	
 	public void levelUp(Quester quester) {
 		Random generator = new Random();
 		int add_health;
 		int size;
 		level++;
-		exp = 0;
+		exp -= 400 * (level);
 		if (!isCombatClass()) return;
 		if (type.equals("Warrior")) {
 			size = 10;
-		} else if (type.equals("Archer") || type.equals("Peace Mage")) {
+		} else if (type.equals("Archer") || type.equals("PeaceMage")) {
 			size = 6;
 		} else {
 			size = 4;
@@ -68,7 +117,11 @@ public class SkillClass {
 		
 		add_health = generator.nextInt(size) + 1;
 		
-		quester.addHealth(add_health);
+		if (isCombatClass()) {
+			quester.addHealth(add_health);
+		}
+
+		quester.getPlayer().sendMessage("Congratulations on becoming a level " + level + " " + type);
 	}
 	
 	private boolean isCombatClass() {
@@ -115,12 +168,21 @@ public class SkillClass {
 	}
 	
 	public boolean isClassItem(int item_id) {
+		int i;
+		
+		for (i = 0; i < ability_list.length; i++) {
+			if (ability_list[i].isBound(item_id)) {
+				return true;
+			}
+		}
+		
 		if (type.equals("Warrior")) { // Warrior
-			return true;
-			/*if (item_id == 272) return true;			// Stone
+			if (item_id == 272) return true;			// Stone
 			else if (item_id == 276) return true;	// Diamond
 			else if (item_id == 283) return true;	// Gold
-			else if (item_id == 267) return true;	// Iron*/
+			else if (item_id == 267) return true;	// Iron
+		} else if (type.equals("Archer")) {
+			if (item_id == 261) return true;
 		} else if (type.equals("Miner")) {
 			if (item_id == 274) return true;			// Stone
 			else if (item_id == 278) return true;	// Diamond
@@ -149,12 +211,32 @@ public class SkillClass {
 		return name;
 	}
 	
-	public void blockDestroy(Player player, Block block) {
-		// TODO: write SkillClass.blockDestroy(player, block);
+	public void blockDestroy(Player player, Block block, Quester quester) {
+		int i;
+		
+		for (i = 0; i < ability_list.length; i++) {
+			if (ability_list[i].isBound(player.getItemInHand())) {
+				ability_list[i].parseLeftClick(player, block, quester);
+				return;
+			}
+		}
+		
+		if (isClassItem(block.getType())) {
+			expAdd(2, quester);
+		} else {
+			expAdd(1, quester);
+		}
 	}
 	
 	public Ability getAbility(String name) {
-		// TODO: write SkillClass.getAbility(name);
+		int i;
+		
+		for (i = 0; i < ability_list.length; i++) {
+			if (ability_list[i].getName().equals(name)) {
+				return ability_list[i];
+			}
+		}
+		
 		return null;
 	}
 
@@ -187,7 +269,7 @@ public class SkillClass {
 			abil_id = results.getInt("abil_list_id");
 			ability_list = abilListSQL(abil_id);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Problem reading Ability");
 			e.printStackTrace();
 		}
 	}
@@ -196,24 +278,25 @@ public class SkillClass {
 		Ability list[];
 		int i;
 		int count = 0;
-		/*ResultSet results = sql_server.query("SELECT * FROM abilities WHERE abil_list_id='" + abilId + "'");
+		ResultSet results = sql_server.query("SELECT * FROM abilities WHERE abil_list_id='" + abilId + "'");
+		
+		results.next();
 		
 		for (i = 0; i < 10; i++) {
-			if (results.getString("abil" + i) != null) {
+			if (!results.getString("abil" + i).equals("0")) {
 				count++;
 			}
 		}
 		list = new Ability[count];
 		
 		for (i = 0; i < count; i++) {
-			int AbilID = Integer.parseInt(results.getString("abil" + i));
-			list[i] = new Ability(getAbilName(AbilID), AbilID);
+			list[i] = new Ability(results.getString("abil" + i));
 		}
 		
-		return list;*/
-		return null;
+		return list;
 	}
 
+	@SuppressWarnings("unused")
 	private String getAbilName(int abilID) throws SQLException {
 		ResultSet results;
 		
@@ -224,6 +307,85 @@ public class SkillClass {
 
 	public String getType() {
 		return type;
+	}
+
+	public void checkEquip(Player player, Inventory equip, Inventory inven) {
+		int item_ids[];
+		int i;
+		
+		if (level > 20) {
+			return;
+		}
+		
+		if (type.equals("Warrior")) {
+			item_ids = new int[4];
+			item_ids[0] = 310;
+			item_ids[1] = 311;
+			item_ids[2] = 312;
+			item_ids[3] = 313;
+		} else if (type.equals("Archer")) {
+			item_ids = new int[4];
+			item_ids[0] = 306;
+			item_ids[1] = 307;
+			item_ids[2] = 308;
+			item_ids[3] = 309;
+		} else if (type.equals("WarMage") || type.equals("PeaceMage")) {
+			item_ids = new int[4];
+			item_ids[0] = 314;
+			item_ids[1] = 315;
+			item_ids[2] = 316;
+			item_ids[3] = 317;
+		} else {
+			return;
+		}
+		
+		for (i = 0; i < item_ids.length; i++) {
+			if (equip.hasItem(item_ids[i], 1, 10000)) {
+				equip.removeItem(new Item(item_ids[i], 1));
+				player.giveItemDrop(item_ids[i], 1);
+				equip.updateInventory();
+				inven.updateInventory();
+				player.sendMessage("You are not high enough level to use that armor");
+			}
+		}
+		
+		if (level <= 2) {
+			item_ids = new int[4];
+			item_ids[0] = 298;
+			item_ids[1] = 299;
+			item_ids[2] = 300;
+			item_ids[3] = 301;
+		}
+
+		for (i = 0; i < item_ids.length; i++) {
+			if (equip.hasItem(item_ids[i], 1, 10000)) {
+				equip.removeItem(new Item(item_ids[i], 1));
+				player.giveItemDrop(item_ids[i], 1);
+				equip.updateInventory();
+				inven.updateInventory();
+				player.sendMessage("You are not high enough level to use that");
+			}
+		}
+		
+		return;
+	}
+
+	public void rightClick(Player player, Block blockClicked, Item item, Quester quester) {
+		int i;
+		
+		for (i = 0; i < ability_list.length; i++) {
+			if (ability_list[i].isBound(player.getItemInHand())) {
+				ability_list[i].parseRightClick(player, blockClicked, quester);
+			}
+		}
+		
+		return;
+	}
+
+	public void display(Player player) {
+		player.sendMessage("You are a level " + level + " " + type + " with " + exp + "/" + (100*(level+1)) + " Exp to next level");
+		
+		return;
 	}	
 	
 	
