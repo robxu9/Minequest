db6382803f8e44e6289ad270b832dd078fd7fdde
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-03-08 22:26:13 +0000

    fixed buying property and added lava to water spell

---
+++ trunk/MineQuest/bukkit/src/org/monk/MineQuest/Ability/AbilityLavaToWater.java
@@ -0,0 +1,65 @@
+package org.monk.MineQuest.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.monk.MineQuest.Quester.Quester;
+import org.monk.MineQuest.Quester.SkillClass.SkillClass;
+
+public class AbilityLavaToWater extends Ability {
+
+	public AbilityLavaToWater(String name, SkillClass myclass) {
+		super(name, myclass);
+	}
+	
+	public int getReqLevel() {
+		return 30;
+	}
+	
+	@Override
+	public List<ItemStack> getManaCost() {
+		List<ItemStack> cost = new ArrayList<ItemStack>();
+		
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		cost.add(new ItemStack(Material.SNOW_BLOCK));
+		
+		return cost;
+	}
+	
+	@Override
+	public String getName() {
+		return "Lava To Water";
+	}
+	
+	@Override
+	public void castAbility(Quester quester, Location location,
+			LivingEntity entity) {
+		Location loc = quester.getPlayer().getLocation();
+		int x, y, z;
+		
+		for (x = (int)(loc.getX() - 15); x < (int)(loc.getX() - 15); x++) {
+			for (y = (int)(loc.getY() - 15); y < (int)(loc.getY() - 15); y++) {
+				for (z = (int)(loc.getZ() - 15); z < (int)(loc.getZ() - 15); z++) {
+					Location block_loc = new Location(loc.getWorld(), x, y, z);
+					
+					if (loc.getWorld().getBlockAt(block_loc).getType() == Material.LAVA) {
+						loc.getWorld().getBlockAt(block_loc).setType(Material.WATER);
+					}
+				}
+			}
+		}
+	}
+
+}
