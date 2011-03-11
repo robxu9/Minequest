109668a43851a95853b1e31936865b1857dc5ef0
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-03-11 05:30:31 +0000

    added ability grow tree

---
+++ trunk/MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilityGrowTrees.java
@@ -0,0 +1,62 @@
+package org.monk.MineQuest.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.TreeType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.monk.MineQuest.Quester.Quester;
+import org.monk.MineQuest.Quester.SkillClass.SkillClass;
+import org.monk.MineQuest.Quester.SkillClass.Resource.Farmer;
+
+public class AbilityGrowTrees extends Ability {
+
+	@Override
+	public void castAbility(Quester quester, Location location,
+			LivingEntity entity) {
+		Location loc = new Location(location.getWorld(), location.getX() - 2,
+				location.getY(), location.getZ() - 2);
+		Location loc_2 = new Location(location.getWorld(), location.getX() - 2,
+				location.getY(), location.getZ() + 2);
+		Location loc_3 = new Location(location.getWorld(), location.getX() + 2,
+				location.getY(), location.getZ() + 2);
+		Location loc_4 = new Location(location.getWorld(), location.getX() + 2,
+				location.getY(), location.getZ() - 2);
+		
+		loc.getWorld().generateTree(loc, TreeType.TREE);
+		loc.getWorld().generateTree(loc_2, TreeType.TREE);
+		loc.getWorld().generateTree(loc_3, TreeType.TREE);
+		loc.getWorld().generateTree(loc_4, TreeType.TREE);
+	}
+
+	@Override
+	public SkillClass getClassType() {
+		return new Farmer();
+	}
+
+	@Override
+	public List<ItemStack> getManaCost() {
+		List<ItemStack> cost = new ArrayList<ItemStack>();
+		
+		cost.add(new ItemStack(Material.SAPLING, 1));
+		cost.add(new ItemStack(Material.SAPLING, 1));
+		cost.add(new ItemStack(Material.SAPLING, 1));
+		cost.add(new ItemStack(Material.SAPLING, 1));
+		
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "Grow Trees";
+	}
+
+	@Override
+	public int getReqLevel() {
+		return 30;
+	}
+
+}
