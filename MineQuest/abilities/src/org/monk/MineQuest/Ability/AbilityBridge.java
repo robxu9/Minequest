b722f7a4482377441071f00b508ea0f29a08e5a5
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-03-11 04:12:58 +0000

    bridge now working

--- MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilityBridge.java
+++ MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilityBridge.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.ItemStack;
 import org.monk.MineQuest.Quester.Quester;
@@ -14,7 +16,40 @@ public class AbilityBridge extends Ability {
 	
 	@Override
 	public List<ItemStack> getManaCost() {
-		return new ArrayList<ItemStack>();
+		List<ItemStack> cost = new ArrayList<ItemStack>();
+		
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		cost.add(new ItemStack(Material.COBBLESTONE, 1));
+		
+		return cost;
 	}
 	
 	@Override
@@ -25,7 +60,46 @@ public class AbilityBridge extends Ability {
 	@Override
 	public void castAbility(Quester quester, Location location,
 			LivingEntity entity) {
+		int x_change = 0;
+		int z_change = 0;
+		int i;
+		double rot = location.getYaw() % 360 - 90;
+		Location loc_1 = new Location(location.getWorld(), (int)location.getX(),
+				(int)(location.getY() - 1), (int)location.getZ());
+		Location loc_2 = new Location(location.getWorld(), (int)location.getX(),
+				(int)(location.getY() - 1), (int)location.getZ());
+		while (rot < 0) rot += 360;
 		
+		if ((rot  < 45) || (rot > 315)) {
+			x_change = -1;
+			loc_2.setZ(loc_2.getZ() + 1);
+		} else if ((rot > 45) && (rot < 135)) {
+			z_change = -1;
+			loc_2.setX(loc_2.getX() + 1);
+		} else if ((rot > 135) && (rot < 225)) {
+			x_change = 1;
+			loc_2.setZ(loc_2.getZ() + 1);
+		} else {
+			z_change = 1;
+			loc_2.setX(loc_2.getX() + 1);
+		}
+		
+		for (i = 0; i < 15; i++) {
+			loc_1.setX(loc_1.getX() + x_change);
+			loc_1.setZ(loc_1.getZ() + z_change);
+			loc_2.setX(loc_2.getX() + x_change);
+			loc_2.setZ(loc_2.getZ() + z_change);
+
+			Block block = loc_1.getWorld().getBlockAt(loc_1);
+			if (block.getType() == Material.AIR) {
+				block.setType(Material.COBBLESTONE);
+			}
+
+			block = loc_2.getWorld().getBlockAt(loc_2);
+			if (block.getType() == Material.AIR) {
+				block.setType(Material.COBBLESTONE);
+			}
+		}
 	}
 
 	@Override
