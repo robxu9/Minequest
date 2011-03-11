5353555038d9131ff80b576a6a0982f2d93294e0
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-03-11 16:28:02 +0000

    added gpl headers

--- trunk/MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilityBridge.java
+++ trunk/MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilityBridge.java
@@ -1,3 +1,21 @@
+/*
+ * MineQuest - Bukkit Plugin for adding RPG characteristics to minecraft
+ * Copyright (C) 2011  Jason Monk
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.monk.MineQuest.Ability;
 
 import java.util.ArrayList;
@@ -92,14 +110,14 @@ public class AbilityBridge extends Ability {
 
 			Block block = loc_1.getWorld().getBlockAt(loc_1);
 			if (block.getType() == Material.AIR) {
-				if ((quester == null) || (quester.getQuest() == null) || (quester.getQuest().canEdit(quester, block))) {
+				if ((quester == null) || (quester.canEdit(block))) {
 					block.setType(Material.COBBLESTONE);
 				}
 			}
 
 			block = loc_2.getWorld().getBlockAt(loc_2);
 			if (block.getType() == Material.AIR) {
-				if ((quester == null) || (quester.getQuest() == null) || (quester.getQuest().canEdit(quester, block))) {
+				if ((quester == null) || (quester.canEdit(block))) {
 					block.setType(Material.COBBLESTONE);
 				}
 			}
--- trunk/MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilitySprintA.java
+++ trunk/MineQuest/abilities/src/org/monk/MineQuest/Ability/AbilitySprintA.java
@@ -1,3 +1,21 @@
+/*
+ * MineQuest - Bukkit Plugin for adding RPG characteristics to minecraft
+ * Copyright (C) 2011  Jason Monk
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package org.monk.MineQuest.Ability;
 
 import java.util.ArrayList;
@@ -38,6 +56,7 @@ public class AbilitySprintA extends Ability {
 	@Override
 	public void castAbility(Quester quester, Location location,
 			LivingEntity entity) {
+		if (quester == null) return;
 		Player player = quester.getPlayer();
 		
 		player.sendMessage("Casting " + getName());
@@ -54,7 +73,7 @@ public class AbilitySprintA extends Ability {
 		} else {
 			loc.setZ(loc.getZ() + 1);
 		}
-		if ((quester.getQuest() == null) || (quester.getQuest().canEdit(quester, location.getWorld().getBlockAt(location)))) {
+		if (quester.canEdit(location.getWorld().getBlockAt(location))) {
 			player.teleportTo(loc);
 		}
 	}
