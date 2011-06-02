bc95e1b59c9983b324cd07d34ea1745fa4099165
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-06-02 20:52:53 +0000

    and these...

---
+++ trunk/release/MineQuestAbilities-0.5/src/org/monk/MineQuest/Ability/AbilityHealthRegeneration.java
@@ -0,0 +1,70 @@
+package org.monk.MineQuest.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.monk.MineQuest.MineQuest;
+import org.monk.MineQuest.Ability.Ability;
+import org.monk.MineQuest.Ability.PassiveAbility;
+import org.monk.MineQuest.Event.AbilityEvent;
+import org.monk.MineQuest.Event.Relative.AuraEvent;
+import org.monk.MineQuest.Quester.Quester;
+
+public class AbilityHealthRegeneration extends Ability implements PassiveAbility {
+	public AbilityHealthRegeneration() {
+		super();
+		config = new int[] {5000, 300000, 1};
+	}
+	
+	@Override
+	public void enable(Quester quester) {
+		super.enable(quester);
+		if (enabled) {
+			MineQuest.getEventQueue().addEvent(new AbilityEvent(config[1], this));
+			MineQuest.getEventQueue().addEvent(new AuraEvent(quester, config[0], config[1], config[2], true, 0));
+		}
+	}
+	
+	@Override
+	public void eventActivate() {
+		super.eventActivate();
+		disable();
+		myclass.getQuester().sendMessage("Health Regeneration Complete!");
+	}
+
+	@Override
+	public void castAbility(Quester quester, Location location,
+			LivingEntity entity) {
+		// Passive
+	}
+
+	@Override
+	public List<ItemStack> getManaCost() {
+		List<ItemStack> list = new ArrayList<ItemStack>();
+
+		list.add(new ItemStack(Material.BREAD, 1));
+		list.add(new ItemStack(Material.BREAD, 1));
+		
+		return list;
+	}
+
+	@Override
+	public String getName() {
+		return "Health Regeneration";
+	}
+
+	@Override
+	public int getReqLevel() {
+		return 20;
+	}
+
+	@Override
+	public String getSkillClass() {
+		return "Warrior";
+	}
+
+}
---
+++ trunk/release/MineQuestAbilities-0.5/src/org/monk/MineQuest/Ability/AbilityLightningArrow.java
@@ -0,0 +1,61 @@
+package org.monk.MineQuest.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.monk.MineQuest.MineQuest;
+import org.monk.MineQuest.Ability.Ability;
+import org.monk.MineQuest.Event.Absolute.BlockCDEvent;
+import org.monk.MineQuest.Quester.Quester;
+
+public class AbilityLightningArrow extends Ability {
+	public AbilityLightningArrow() {
+		config = new int[] {2, 2};
+	}
+
+	@Override
+	public void castAbility(Quester quester, Location location,
+			LivingEntity entity) {
+		location.getWorld().strikeLightning(location);
+		MineQuest.getEventQueue().addEvent(new BlockCDEvent(10, 30000, 
+				location.getBlock(), Material.FIRE));
+		
+		if (entity != null) {
+			int level = MineQuest.getAdjustment();
+			if (myclass != null) {
+				level = myclass.getCasterLevel() / config[1];
+			}
+			
+			MineQuest.damage(entity, level + config[0]);
+		}
+	}
+
+	@Override
+	public List<ItemStack> getManaCost() {
+		List<ItemStack> list = new ArrayList<ItemStack>();
+
+		list.add(new ItemStack(Material.GLOWSTONE_DUST, 3));
+		
+		return list;
+	}
+
+	@Override
+	public String getName() {
+		return "LightningArrow";
+	}
+
+	@Override
+	public int getReqLevel() {
+		return 18;
+	}
+
+	@Override
+	public String getSkillClass() {
+		return "Archer";
+	}
+
+}
