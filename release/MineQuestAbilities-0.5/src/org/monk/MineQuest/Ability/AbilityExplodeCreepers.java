7f22f6e0df1caa1cd1432b3554c382c5e9e5f3c6
jmonk@dream-crushed.com <jmonk@dream-crushed.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-06-02 20:52:53 +0000

    and these...

---
+++ release/MineQuestAbilities-0.5/src/org/monk/MineQuest/Ability/AbilityExplodeCreepers.java
@@ -0,0 +1,68 @@
+package org.monk.MineQuest.Ability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.ItemStack;
+import org.monk.MineQuest.MineQuest;
+import org.monk.MineQuest.Ability.Ability;
+import org.monk.MineQuest.Event.Absolute.HealthEvent;
+import org.monk.MineQuest.Event.Relative.ExplosionEvent;
+import org.monk.MineQuest.Quester.Quester;
+
+public class AbilityExplodeCreepers extends Ability {
+	public AbilityExplodeCreepers() {
+		super();
+		config = new int[] {15, 4, 2};
+	}
+
+	@Override
+	public void castAbility(Quester quester, Location location,
+			LivingEntity entity) {
+		World world;
+		if (quester != null) {
+			world = quester.getPlayer().getWorld();
+		} else {
+			world = entity.getWorld();
+		}
+		
+		for (LivingEntity lentity : getEntities((quester != null)?quester.getPlayer():entity, config[0])) {
+			if (lentity instanceof Creeper) {
+				MineQuest.getEventQueue().addEvent(new ExplosionEvent(10, world, lentity, 0, 0, 0, config[1], config[2]));
+				MineQuest.getEventQueue().addEvent(new HealthEvent(20, lentity, 0));
+			}
+		}
+	}
+
+	@Override
+	public List<ItemStack> getManaCost() {
+		List<ItemStack> cost = new ArrayList<ItemStack>();
+		
+		cost.add(new ItemStack(Material.SULPHUR, 1));
+		cost.add(new ItemStack(Material.SULPHUR, 1));
+		cost.add(new ItemStack(Material.SULPHUR, 1));
+		
+		return cost;
+	}
+
+	@Override
+	public String getName() {
+		return "Explode Creeper";
+	}
+
+	@Override
+	public int getReqLevel() {
+		return 15;
+	}
+
+	@Override
+	public String getSkillClass() {
+		return "PeaceMage";
+	}
+
+}
