ec17c4d4ca9a01e14f15fda07b8aa8b74d2e2ef0
jmonk@theminequest.com <jmonk@theminequest.com@b9554a65-ab59-cc2f-58ea-6c691e74d95b>
2011-06-16 23:43:22 +0000

    trying to catch up to 0.5 changes, i kind of screwed up...

--- dev/MineQuest/src/org/monk/MineQuest/Ability/BreakingAbility.java
+++
@@ -1,8 +0,0 @@
-package org.monk.MineQuest.Ability;
-
-import org.bukkit.block.Block;
-import org.monk.MineQuest.Quester.Quester;
-
-public interface BreakingAbility {
-	public void blockBreak(Quester quester, Block block);
-}
